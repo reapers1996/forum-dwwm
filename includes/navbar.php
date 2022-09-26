aabe130ce650c686ee879d4674a119ef89e7dce3
reapers1996 <damien.forest.via@gmail.com>
2022-09-26 14:39:34 +0200

    d
    d

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Forum</a>
+    <a class="navbar-brand" href="https://www.secourspopulaire.fr/"><img src="includes/logo.png" width="60px" height="60px"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="index.php">Les questions</a>
+          <a class="nav-link" href="index.php">Accueil Forum</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="publish-question.php">Publier une question</a>
@@ -17,6 +17,9 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="login.php">Connexion</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://fr-fr.facebook.com/CopainduMonde13/"><img src="includes/logocopaindumonde.png" width="60px" height="60px"></a>
         </li>
         <?php 
           if(isset($_SESSION['auth'])){
